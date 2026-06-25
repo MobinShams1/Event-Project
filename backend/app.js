692bd3c4fe953e7ff35e8730b83dcff5613b5411
Mobin <themobin2003@gmail.com>
2026-06-25 13:55:31 +0330

    Add .gitignore for project

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With, content-type, X-Username, X-UserId'  // ✅ اضافه کردن هدرهای جدید
+    'X-Requested-With, content-type, X-Username, X-UserId' 
   );
   next();
 });
